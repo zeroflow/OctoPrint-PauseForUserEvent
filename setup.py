a1ffcc301e4b65b7c7a7000abad950a3ebf09e6a
Thomas Arthofer <arthofer.thomas@gmail.com>
2019-06-04 16:45:30 +0200

    Change plugin identifier
    Use all lowercase

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "PauseForUserEvent"
+plugin_identifier = "pause_for_user_event"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_PauseForUserEvent"
+plugin_package = "octoprint_pause_for_user_event"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
