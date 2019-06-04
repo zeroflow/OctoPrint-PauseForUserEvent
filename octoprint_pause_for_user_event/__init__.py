6dea103cd139334275e43f80edb83e370fca2b7d
Thomas Arthofer <arthofer.thomas@gmail.com>
2019-06-04 17:03:31 +0200

    Remove additional classes, clean up, bump version

--- octoprint_pause_for_user_event/__init__.py
+++ octoprint_pause_for_user_event/__init__.py
@@ -1,32 +1,14 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 import logging
 from octoprint.events import eventManager, Events
 
-class PauseForUserEvent(octoprint.plugin.SettingsPlugin,
-                            octoprint.plugin.AssetPlugin,
-                            octoprint.plugin.TemplatePlugin):
+class PauseForUserEvent(octoprint.plugin.SettingsPlugin):
     
     def get_settings_defaults(self):
         return {}
-
-    ##~~ AssetPlugin mixin
-
-    def get_assets(self):
-        # Define your plugin's asset files to automatically include in the
-        # core UI here.
-        return {}
-        
         
     def get_update_information(self):
         # Define the configuration for your plugin to use with the Software Update
@@ -51,6 +33,9 @@ class PauseForUserEvent(octoprint.plugin.SettingsPlugin,
     def on_after_startup(self):
         self.triggered = False
         None
+        
+    def register_custom_events(self, *args, **kwargs):
+        return ["notify"]
     
     def recv_callback(self, comm_instance, line, *args, **kwargs):
         # Found keyword, fire event and block until other text is received
@@ -63,10 +48,6 @@ class PauseForUserEvent(octoprint.plugin.SettingsPlugin,
             self.triggered = False
             
         return line
-    
-    def register_custom_events(self, *args, **kwargs):
-        return ["notify"]
-
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
